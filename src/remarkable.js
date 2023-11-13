c8bdfff2fae1bfd218220667485d68eb91fbd5a5
Given Ncube <givenyslim12@gmail.com>
2023-11-13 13:26:35 +0200

    fix: used the correct header

--- src/remarkable.js
+++ src/remarkable.js
@@ -3,7 +3,7 @@ import { codes, statuses } from './config.js';
 export const remarkable = (error, req, res, next) => {
     if (error.message === codes.UNPROCESSABLE_ENTITY) {
         //if the clients expects json return json else redirect back with errors in session and old input
-        if (req.accepts('json') || req.get('X-Requested-With') === 'XMLHttpRequest' || req.is('application/json')) {
+        if (req.accepts('application/json') || req.get('X-Requested-With') === 'XMLHttpRequest' || req.is('application/json')) {
             return res.status(statuses.UNPROCESSABLE_ENTITY).json({
                 message: error.message,
                 errors: error.errors,
